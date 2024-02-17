6cd46f0112a20f32c1a7e1fce65537054e85bcc8
HugoVeraGarcia <hugo.vera.garcia@gmail.com>
2024-02-17 16:38:20 -0500

    first commit

---
+++ src/store.js
@@ -0,0 +1,20 @@
+import { proxy } from 'valtio'
+
+const state = proxy({
+  intro: true,
+  colors: [
+    '#ccc',
+    '#EFBD4E',
+    '#80C670',
+    '#726DE8',
+    '#EF674E',
+    '#353934',
+    'cyan'
+  ],
+  // decals: ['react', 'three2', 'pmndrs', 'hugo_malo1', 'la_raza'],
+  decals: ['hugo_malo1', 'la_raza','hugo_malo2', 'german', 'el_senior', 'jesus'],
+  selectedColor: '#EFBD4E',
+  selectedDecal: 'hugo_malo1'
+})
+
+export { state }
